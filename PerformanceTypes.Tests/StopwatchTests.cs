5fbfb22ef060d2eeb84378ea26300316e9743eef
Bret Copeland <bret@atlantisflight.org>
2017-02-18 12:31:20 -0500

    Add test for StopwatchStruct

---
+++ PerformanceTypes.Tests/StopwatchTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using NUnit.Framework;
+
+namespace PerformanceTypes.Tests
+{
+    [TestFixture]
+    public class StopwatchTests
+    {
+        [Test]
+        public void ResultMatchesStopwatch()
+        {
+            var sw = Stopwatch.StartNew();
+            var ss = new StopwatchStruct();
+            ss.Start();
+
+            Thread.Sleep(500);
+
+            sw.Stop();
+            ss.Stop();
+
+            var swMs = sw.Elapsed.TotalMilliseconds;
+            var ssMs = ss.GetElapsedMilliseconds();
+
+            var difference = Math.Abs(swMs - ssMs);
+
+            Assert.IsTrue(difference < 1);
+        }
+    }
+}
